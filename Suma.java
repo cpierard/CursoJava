ed73a04250796b70dfb7f143058217177ee70924
Claudio Pierard <cl.pierard@gmail.com>
2017-01-10 14:08:57 -0600

    agrego archivo suma.java , segundo ejercicio 1era clase.

---
+++ Suma.java
@@ -0,0 +1,18 @@
+public class Suma {
+
+  public static void main(String[] args) {
+
+    // Declaración de variables
+    int x = 10;
+    int y = 5;
+    int suma = 0;
+
+    suma = x + y;
+
+    System.out.println("La suma de " + x + " y " + y + " es " + suma);
+
+    String cad = "Hola Mundo";
+    System.out.println(cad);
+  }
+
+}
