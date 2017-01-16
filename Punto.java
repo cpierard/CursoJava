4caf1f40bbcc895ee3efcafc324858107f78931a
Claudio Pierard <cl.pierard@gmail.com>
2017-01-16 15:58:11 -0600

    Agrego lo que faltaba

--- Punto.java
+++ Punto.java
@@ -9,6 +9,12 @@ public class Punto {
     this.y = y;
 
   }
+//Método vacío para el constructor
+
+public Punto(){
+
+}
+
 
 public String toString(){
 
@@ -23,6 +29,17 @@ public double distancia(Punto p_1, Punto p_2){
   double d = Math.sqrt( Math.pow( p_2.x - p_1.x, 2) + Math.pow( p_2.y - p_1.y, 2));
 
   return d;
+}
+
+//Método que calcula la distancia de un punto a otro
+//Un punto pi calcula su distancia a otro pj
+
+public double distancia(Punto pj) {
+
+  double d = Math.sqrt( Math.pow( pj.x - this.x, 2) + Math.pow( pj.y - this.y, 2));
+
+  return d;
+
 }
 
 }
--- UsoPunto.java
+++ UsoPunto.java
@@ -4,13 +4,19 @@ public class UsoPunto {
 
     Punto p1 = new Punto(1,2);
     Punto p2 = new Punto(-4,6);
+    Punto interviene = new Punto();
 
     System.out.println(p1);
     System.out.println(p2);
 
-    double resDistancia = p1.distancia(p1, p2);
+    double resDistancia = interviene.distancia(p1, p2);
+
+    double distDos = p1.distancia(p2);
 
     System.out.println("La distancia de " + p1 + " a " + p2 + " es " + resDistancia);
+    System.out.println(distDos);
+
+    System.out.println(p1.distancia(p2));
 
   }
 }
